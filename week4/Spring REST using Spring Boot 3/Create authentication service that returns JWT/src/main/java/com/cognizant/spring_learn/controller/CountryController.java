f2d2aade4f9a1fe7b7b7da90304a0a8952c95548
Muskan <muskansinha803101@gmail.com>
2025-07-13 23:36:35 +0530

    Week 4

---
+++ week4/Spring REST using Spring Boot 3/Create authentication service that returns JWT/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -0,0 +1,24 @@
+package com.cognizant.spring_learn.controller;
+
+import com.cognizant.spring_learn.model.Country;
+import com.cognizant.spring_learn.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+        org.springframework.context.ApplicationContext context = new org.springframework.context.support.ClassPathXmlApplicationContext("spring.xml");
+        return (Country) context.getBean("in");
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        return countryService.getCountry(code);
+    }
+}
